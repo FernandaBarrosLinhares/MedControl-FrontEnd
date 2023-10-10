874c4d7beff0b41ab60e8592e23cfc25971635ff
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-10-10 15:57:00 -0300

    Base do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+    HttpClientModule,
+  ],
+  providers: [provideNgxMask()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
