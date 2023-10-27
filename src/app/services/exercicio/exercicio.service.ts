f7019507764556b8e6b3673161f28c1624ab7310
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-27 13:03:12 -0300

    Ajustes nas funções e toast

--- src/app/services/exercicio/exercicio.service.ts
+++ src/app/services/exercicio/exercicio.service.ts
@@ -1,8 +1,10 @@
 import { IExercicio } from '../../interfaces/IExercicio';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 import IPaciente from '../../interfaces/IPaciente';
+import { LoginService } from '../login/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
@@ -11,49 +13,96 @@ export class ExercicioService {
   urlBase = 'http://localhost:4200/api/exercicios';
   exercicios: IExercicio[] = [];
 
-  constructor(private httpClient: HttpClient) {}
-
-  // TODO apagar abaixo quando tiver o service de paciente
-  async buscarTodosPacientes() {
-    return await lastValueFrom(
-      this.httpClient.get<IPaciente[]>('http://localhost:4200/api/pacientes')
-    );
-  }
+  constructor(private httpClient: HttpClient,
+    private loginService: LoginService,
+    private toastr: ToastrService
+    ) {}
 
   async buscarTodos() {
-    this.exercicios = await lastValueFrom(
-      this.httpClient.get<IExercicio[]>(this.urlBase)
-    );
+    try{
+      this.exercicios = await lastValueFrom(this.httpClient.get<IExercicio[]>(`${this.urlBase}`));
+    }catch(e:any){
+      if(e.error[0].mensagem){
+        this.toastr.error(e.error[0].mensagem,'Erro ao Buscar');
+      }else if(e.error){
+        this.toastr.error(e.error,'Erro ao Buscar');
+      }
+      return null;
+    }
     return this.exercicios;
   }
 
   async buscarPorId(id: number) {
-    return await lastValueFrom(
-      this.httpClient.get(`${this.urlBase}/${id}`)
-    ).catch((e) => {
-      console.log(e.error);
+    try {
+      return await lastValueFrom(this.httpClient.get(`${this.urlBase}/${id}`));
+    } catch (e: any) {
+      if(e.error[0].mensagem){
+        this.toastr.error(e.error[0].mensagem,'Erro ao Buscar');
+      }else if(e.error){
+        this.toastr.error(e.error,'Erro ao Buscar');
+      }
       return null;
-    });
+    }
   }
 
   async salvar(exercicio: IExercicio) {
+    const idUsuarioLogado = this.loginService.idUsuarioLogado();
+    if (idUsuarioLogado === undefined) return;
+    let headers = new HttpHeaders().set('idUsuarioLogado', `${idUsuarioLogado}`);
+
     try {
-      return await lastValueFrom(
-        this.httpClient.post(`${this.urlBase}`, exercicio)
+      const response =  await lastValueFrom(
+        this.httpClient.post(`${this.urlBase}`, exercicio, { headers })
       );
-    } catch (e) {
+      this.toastr.success('Cadastro realizado com sucesso','Cadastrado');
+      return response;
+    } catch (e: any) {
+      if(e.error[0].mensagem){
+        this.toastr.error(e.error[0].mensagem,'Erro ao cadastrar');
+      }else if(e.error){
+        this.toastr.error(e.error,'Erro ao cadastrar');
+      }
       return e;
     }
   }
 
-  async editar(exercicio: IExercicio) {
-    return await this.httpClient.put(
-      `${this.urlBase}/${exercicio.id}`,
-      exercicio
-    );
+  async editar(exercicio: IExercicio,exercicioId: number) {
+    const idUsuarioLogado = this.loginService.idUsuarioLogado();
+    if (idUsuarioLogado === undefined) return;
+    let headers = new HttpHeaders().set('idUsuarioLogado', `${idUsuarioLogado}`);
+
+    try{
+      let response = await firstValueFrom(this.httpClient.put<any>(`${this.urlBase}/${exercicioId}`,exercicio,{headers:headers}));
+      this.toastr.success('Atualizado com sucesso','Atualizado');
+      return response;
+    }catch(e:any){
+      if(e.error[0].mensagem){
+        this.toastr.error(e.error[0].mensagem,'Erro ao Atualizar');
+      }else if(e.error){
+        this.toastr.error(e.error,'Erro ao Atualizar');
+      }
+      return e;
+    }
   }
 
   async excluir(id: number) {
-    await this.httpClient.delete(`${this.urlBase}/${id}`);
+    const idUsuarioLogado = this.loginService.idUsuarioLogado();
+    if (idUsuarioLogado === undefined) return;
+    let headers = new HttpHeaders().set('idUsuarioLogado', `${idUsuarioLogado}`);
+
+    try {
+      const response = await lastValueFrom(
+        this.httpClient.delete(`${this.urlBase}/${id}`, { headers }));
+        this.toastr.success('Cadastro Deletado com sucesso','Deletado');
+      return response;
+    }catch(e:any){
+      if(e.error[0].mensagem){
+        this.toastr.error(e.error[0].mensagem,'Erro ao Deltar');
+      }else if(e.error){
+        this.toastr.error(e.error,'Erro ao Deletar');
+      }
+      return null;
+    }
   }
+
 }
